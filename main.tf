51474b1b9cee0939b79556af7b748f8b90f808a5
Chinnu891 <chinnusali891@gmail.com>
2026-06-26 11:57:52 +0530

    .

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     # source = "../terraform-aws-vpc" 
-    source = "git::https://github.com/DevSecOps-AiOps/vpc-test.git?ref=main"
+    source = "git::https://github.com/DevSecOps-AiOps/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
